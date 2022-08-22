efb2bb4dbd97d9d9d0ae4f4c4336fc34b925848b
Dima Lipin <dimich3d@ya.ru>
2022-08-22 10:37:26 +0200

    floating black square

--- data/page/content.js
+++ data/page/content.js
@@ -1,10 +1,45 @@
-function validate() {
-  chrome.runtime.sendMessage({}, res => {})
-  
-}
+function block() {
+  const id = "banner";
+  if (!document.getElementById(id)) {
+    const node = document.createElement("div");
+    node.id = id;
 
-chrome.runtime.onMessage.addListener(
-  request => {
-    console.log(request);
+    document.body.appendChild(node);
   }
-)
+  let node = document.getElementById(id)
+  let xPos = 0;
+  setInterval(() => {
+    xPos++;
+    node.style.left = 10 * xPos + "px";
+    if (node.getBoundingClientRect().x > window.innerWidth) {
+      console.log("oob!");
+      node.x = 0;
+      xPos = 0;
+    }
+  }, 35)
+}
+function validate() {
+  console.log("validating");
+  chrome.storage.local.get({ blocked: [] }, (result) => {
+    let href = location.href.toString();
+    console.log("result", result);
+    if (result.blocked.length > 0) {
+      result.blocked.forEach((blockedSite) => {
+        console.log("trying", blockedSite, "for", href);
+        if (href.includes(blockedSite)) {
+          console.log("site blocked: ", href);
+          block();
+        }
+      });
+    }
+  });
+}
+document.addEventListener("DOMContentLoaded", function (event) {
+  validate();
+});
+
+// chrome.runtime.onMessage.addListener((request) => {
+//   if (request.method === "address-changed") {
+//     validate();
+//   }
+// });
--- worker.js
+++ worker.js
@@ -1,3 +1,14 @@
-chrome.action.onClicked.addListener(
-  tab => console.log(tab)
-)
+chrome.action.onClicked.addListener((tab) => console.log(tab));
+
+chrome.tabs.onUpdated.addListener((tabId, info) => {
+  if (info.url) {
+    console.log("Tab updated:", tabId, info);
+    chrome.tabs.sendMessage(
+      tabId,
+      {
+        method: "address-changed",
+      },
+      () => chrome.runtime.lastError
+    );
+  }
+});
